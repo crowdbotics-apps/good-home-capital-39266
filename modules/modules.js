078b15bc9f0a63cc57ebdbfda3f34ba388068159
Erwin Mothoa <erwin.mothoa@crowdbotics.com>
2023-10-18 14:20:30 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- modules/modules.js
+++ modules/modules.js
@@ -1,70 +1,73 @@
 import React from 'react';
-import { SafeAreaView, View, Text, Image, FlatList, StyleSheet } from 'react-native';
-const dummyData = [{
-  id: '1',
-  cardName: 'Card 1',
-  features: 'Feature 1, Feature 2',
-  benefits: 'Benefit 1, Benefit 2',
-  annualFee: '$100',
-  interestRate: '15%',
-  rewards: 'Reward 1, Reward 2',
-  creditLimit: '$5000'
-} // Add more data as needed
-];
+import { SafeAreaView, StyleSheet, View, Text, Image, FlatList } from 'react-native';
 
 const CreditCardOffersScreen = () => {
+  const dummyData = [{
+    id: '1',
+    cardName: 'Card 1',
+    cardDetails: '0% interest for the first year',
+    cardImage: 'https://tinyurl.com/42evm3m3'
+  }, {
+    id: '2',
+    cardName: 'Card 2',
+    cardDetails: '5% cash back on groceries',
+    cardImage: 'https://tinyurl.com/42evm3m3'
+  }, {
+    id: '3',
+    cardName: 'Card 3',
+    cardDetails: '2x points on travel',
+    cardImage: 'https://tinyurl.com/42evm3m3'
+  }];
+
+  const renderItem = ({
+    item
+  }) => <View style={styles.card}>
+      <Image source={{
+      uri: item.cardImage
+    }} style={styles.cardImage} />
+      <Text style={styles.cardName}>{item.cardName}</Text>
+      <Text style={styles.cardDetails}>{item.cardDetails}</Text>
+    </View>;
+
   return <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Product Recommendations</Text>
-      <FlatList data={dummyData} keyExtractor={item => item.id} renderItem={({
-      item
-    }) => <View style={styles.card}>
-            <Image style={styles.image} source={{
-        uri: 'https://tinyurl.com/42evm3m3'
-      }} />
-            <Text style={styles.cardName}>{item.cardName}</Text>
-            <Text style={styles.details}>Features: {item.features}</Text>
-            <Text style={styles.details}>Benefits: {item.benefits}</Text>
-            <Text style={styles.details}>Annual Fee: {item.annualFee}</Text>
-            <Text style={styles.details}>Interest Rate: {item.interestRate}</Text>
-            <Text style={styles.details}>Rewards: {item.rewards}</Text>
-            <Text style={styles.details}>Credit Limit: {item.creditLimit}</Text>
-          </View>} />
+      <Text style={styles.title}>Credit Card Offers</Text>
+      <FlatList data={dummyData} renderItem={renderItem} keyExtractor={item => item.id} />
     </SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
-    backgroundColor: '#f5f5f5'
+    backgroundColor: '#F5F5F5',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
     color: '#333',
-    marginBottom: 10
+    marginBottom: 20
   },
   card: {
     backgroundColor: '#fff',
-    marginBottom: 10,
-    padding: 10,
-    borderRadius: 10
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 20,
+    alignItems: 'center'
   },
-  image: {
-    width: '100%',
-    height: 200,
-    borderRadius: 10
+  cardImage: {
+    width: 100,
+    height: 100,
+    marginBottom: 10
   },
   cardName: {
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#333',
-    marginTop: 10
+    color: '#333'
   },
-  details: {
-    fontSize: 14,
-    color: '#666',
-    marginTop: 5
+  cardDetails: {
+    fontSize: 16,
+    color: '#666'
   }
 });
 export default CreditCardOffersScreen;
