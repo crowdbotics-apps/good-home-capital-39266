87b45d7ad1faa509cedf44d57af75a9eba5ac3a3
Erwin Mothoa <erwin.mothoa@crowdbotics.com>
2023-10-18 16:24:46 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/BusinessProfileScreen/index.js
+++ screens/BusinessProfileScreen/index.js
@@ -3,36 +3,36 @@ import React from 'react';
 import { SafeAreaView, View, Text, TextInput, Button, Image, ScrollView } from 'react-native';
 
 const BusinessProfileScreen = () => {
-  return <SafeAreaView style={_styles.ENgxcHnx}>
+  return <SafeAreaView style={_styles.EhGaQjaw}>
       <ScrollView>
-        <View style={_styles.pBJumRYL}>
-          <Text style={_styles.iAWaRIAE}>General Information</Text>
-          <TextInput placeholder="Business Name" style={_styles.DRqhnGBP} />
-          <TextInput placeholder="Legal Structure" style={_styles.dTJMdKHI} />
-          <TextInput placeholder="Registration Details" style={_styles.NNIJTFlT} />
-          <TextInput placeholder="Description" style={_styles.WJupxkWR} />
+        <View style={_styles.KtWpvNkK}>
+          <Text style={_styles.TbLkLmgY}>General Information</Text>
+          <TextInput placeholder="Business Name" style={_styles.rXynXIfH} />
+          <TextInput placeholder="Legal Structure" style={_styles.nIJQDruy} />
+          <TextInput placeholder="Registration Details" style={_styles.FdfDpXgk} />
+          <TextInput placeholder="Description" style={_styles.BMMIkYJj} />
 
-          <Text style={_styles.pWNiGsyI}>Contacts and Addresses</Text>
-          <TextInput placeholder="Email Address" style={_styles.iaOmvKzg} />
-          <TextInput placeholder="Phone Number" style={_styles.giLVsMgT} />
-          <TextInput placeholder="Physical Address" style={_styles.hllkzcQp} />
+          <Text style={_styles.IwCOaHRb}>Contacts and Addresses</Text>
+          <TextInput placeholder="Email Address" style={_styles.eGyDilRF} />
+          <TextInput placeholder="Phone Number" style={_styles.XjznGPHe} />
+          <TextInput placeholder="Physical Address" style={_styles.sUCXkcsX} />
 
-          <Text style={_styles.qxGZrUqi}>Licenses</Text>
+          <Text style={_styles.cjOoiaNS}>Licenses</Text>
           <Button title="Upload License" onPress={() => {}} />
 
-          <Text style={_styles.xbzsoKpk}>Departments</Text>
-          <TextInput placeholder="Department Name" style={_styles.BfeHGnTe} />
+          <Text style={_styles.aPXxXSLK}>Departments</Text>
+          <TextInput placeholder="Department Name" style={_styles.wTLODkbf} />
 
-          <Text style={_styles.aMuEJNWQ}>Logos</Text>
+          <Text style={_styles.KWlbqbLK}>Logos</Text>
           <Image source={{
           uri: 'https://tinyurl.com/42evm3m3'
-        }} style={_styles.MIvhsSKK} />
+        }} style={_styles.SnpbOOSJ} />
 
-          <Text style={_styles.grvRWUtN}>Payee Information</Text>
-          <TextInput placeholder="Bank Account Details" style={_styles.pEPmlxKb} />
-          <TextInput placeholder="Payment Preferences" style={_styles.MZNEOLtk} />
+          <Text style={_styles.eSbdGhNJ}>Payee Information</Text>
+          <TextInput placeholder="Bank Account Details" style={_styles.UuVjmzmB} />
+          <TextInput placeholder="Payment Preferences" style={_styles.KkPBWFZY} />
 
-          <Button title="Save" onPress={() => {}} style={_styles.OvHOIpaX} />
+          <Button title="Save" onPress={() => {}} style={_styles.mSPvHtov} />
         </View>
       </ScrollView>
     </SafeAreaView>;
@@ -41,108 +41,108 @@ const BusinessProfileScreen = () => {
 export default BusinessProfileScreen;
 
 const _styles = StyleSheet.create({
-  ENgxcHnx: {
+  EhGaQjaw: {
     flex: 1,
     backgroundColor: "#f5f5f5"
   },
-  pBJumRYL: {
+  KtWpvNkK: {
     padding: 20
   },
-  iAWaRIAE: {
+  TbLkLmgY: {
     fontSize: 24,
     fontWeight: "bold"
   },
-  DRqhnGBP: {
+  rXynXIfH: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  dTJMdKHI: {
+  nIJQDruy: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  NNIJTFlT: {
+  FdfDpXgk: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  WJupxkWR: {
+  BMMIkYJj: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  pWNiGsyI: {
+  IwCOaHRb: {
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 20
   },
-  iaOmvKzg: {
+  eGyDilRF: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  giLVsMgT: {
+  XjznGPHe: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  hllkzcQp: {
+  sUCXkcsX: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  qxGZrUqi: {
+  cjOoiaNS: {
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 20
   },
-  xbzsoKpk: {
+  aPXxXSLK: {
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 20
   },
-  BfeHGnTe: {
+  wTLODkbf: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  aMuEJNWQ: {
+  KWlbqbLK: {
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 20
   },
-  MIvhsSKK: {
+  SnpbOOSJ: {
     width: 100,
     height: 100,
     marginTop: 10
   },
-  grvRWUtN: {
+  eSbdGhNJ: {
     fontSize: 24,
     fontWeight: "bold",
     marginTop: 20
   },
-  pEPmlxKb: {
+  UuVjmzmB: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  MZNEOLtk: {
+  KkPBWFZY: {
     borderWidth: 1,
     borderColor: "#ddd",
     padding: 10,
     marginTop: 10
   },
-  OvHOIpaX: {
+  mSPvHtov: {
     marginTop: 20
   }
 });
