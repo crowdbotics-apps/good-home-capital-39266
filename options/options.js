6fae94d0b1e04b5d7654461c57d23788103da872
Erwin Mothoa <erwin.mothoa@crowdbotics.com>
2023-10-18 11:19:43 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "wispy-block-39266",
-  url: "https://wispy-block-39266.botics.co",
-  api: "https://wispy-block-39266.botics.co/api/v1"
+  "name": "wispy-block-39266",
+  "url": "https://wispy-block-39266.botics.co",
+  "api": "https://wispy-block-39266.botics.co/api/v1",
+  "initialRoute": "CollectPersonalInformation"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
