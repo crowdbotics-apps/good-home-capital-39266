186788b98cbac4f651a15fac00cc4585b037e126
Erwin Mothoa <erwin.mothoa@crowdbotics.com>
2023-10-18 11:18:25 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/LoanInformationScreen/index.js
+++ screens/LoanInformationScreen/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { SafeAreaView, View, Text, Image, Button, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, Image, Button, StyleSheet, TouchableOpacity } from "react-native";
 
 const LoanScreen = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const RadioButton = ({
+    title,
+    onPress,
+    selected
+  }) => {
+    return <TouchableOpacity style={styles.radioButton} onPress={onPress}>
+        <View style={selected ? styles.radioButtonIconSelected : styles.radioButtonIcon} />
+        <Text style={styles.text}>{title}</Text>
+      </TouchableOpacity>;
+  };
+
   return <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Select loan programme</Text>
       </View>
       <View style={styles.content}>
         <Image style={styles.image} source={{
-        uri: 'https://tinyurl.com/42evm3m3'
+        uri: "https://tinyurl.com/42evm3m3"
       }} />
-        <Text style={styles.text}>Conventional</Text>
-        <Text style={styles.text}>Bank Statement</Text>
-        <Text style={styles.text}>Real estate investor</Text>
+        <RadioButton title="Conventional" onPress={() => setSelectedOption("Conventional")} selected={selectedOption === "Conventional"} />
+        <RadioButton title="Bank Statement" onPress={() => setSelectedOption("Bank Statement")} selected={selectedOption === "Bank Statement"} />
+        <RadioButton title="Real estate investor" onPress={() => setSelectedOption("Real estate investor")} selected={selectedOption === "Real estate investor"} />
       </View>
       <View style={styles.footer}>
         <Button title="Back" onPress={() => {}} />
@@ -24,21 +37,21 @@ const LoanScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5F5F5'
+    backgroundColor: "#F5F5F5"
   },
   header: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
   content: {
     flex: 3,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   image: {
     width: 200,
@@ -47,13 +60,37 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 18,
-    marginBottom: 10
+    marginLeft: 10
   },
   footer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  radioButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 10
+  },
+  radioButtonIcon: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  radioButtonIconSelected: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000"
   }
 });
 export default LoanScreen;
